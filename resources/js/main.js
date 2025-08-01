1679d12e675558ebef3704dab1115c79696020e1
eemi1 <emip1891@gmail.com>
2025-08-01 02:19:37 -0300

    CSS Hecho de la página principal y testeo de CSS Responsive.

---
+++ resources/js/main.js
@@ -0,0 +1,12 @@
+const menuButton = document.getElementById('menu-button');
+const dropdownMenu = document.getElementsByClassName('dropdown-menu-a');
+
+menuButton.addEventListener('click', () => {
+    dropdownMenu.style.display='flex';
+});
+const icon = document.getElementById('icon-user');
+
+icon.addEventListener('click', (e) => {
+
+
+})
